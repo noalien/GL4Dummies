eaa033a8573fbdef9e8011e66c275ae809e80417
Phuntsok Drak-pa <phundrak@phundrak.fr>
2018-05-21 03:51:37 +0200

    Ajout de documentation pour la structure `list`

--- lib_src/GL4D/list.c
+++ lib_src/GL4D/list.c
@@ -2,125 +2,182 @@
  * \brief Fonctions de gestion de liste.
  * \author Kevin HAVRANEK mzartek@hotmail.fr
  * \date Mars 17, 2015
- *
  */
 
 #include "list.h"
 #include <string.h>
 #include <assert.h>
 
+/**
+ *  Créé une liste vide d’éléments de taille \p data_size et renvoie un pointeur
+ *  sur ladite liste
+ *
+ *  \param data_size
+ *  \return Retourne un pointeur sur la liste créée
+ */
 list_t list_new(size_t data_size) {
-	list_t list;
+  list_t list;
 
-	list.begin = malloc(sizeof *list.begin);
-	list.size = 0;
-	list.data_size = data_size;
+  list.begin = malloc(sizeof *list.begin);
+  list.size = 0;
+  list.data_size = data_size;
 
-	list.begin->data = malloc(data_size);
-	list.begin->next = NULL;
+  list.begin->data = malloc(data_size);
+  list.begin->next = NULL;
 
-	assert(list.begin);
-	assert(list.begin->data);
+  assert(list.begin);
+  assert(list.begin->data);
 
-	return list;
+  return list;
 }
 
+/**
+ *  Supprime chaque élément de la liste \p list avant de détruire la liste
+ *  elle-même.\n
+ *  Attention ! Si les éléments stockés doivent être détruits via un destructeur
+ *  spécial, ce dernier ne sera pas exécuté lors de l’appel de cette fonction !
+ *
+ *  \param list
+ */
 void list_delete(list_t list) {
-	struct _list_t *current, *next;
+  struct _list_t *current, *next;
 
-	for (current = list.begin; current != NULL; current = next) {
-		next = current->next;
-		free(current->data);
-		free(current);
-	}
+  for (current = list.begin; current != NULL; current = next) {
+    next = current->next;
+    free(current->data);
+    free(current);
+  }
 }
 
+/**
+ *  Insère un nouvel élément \p el dans la liste \p list à l’index \p index,
+ *  avant l’élément se trouvant précédemment à ce même index (ce dernier et les
+ *  éléments suivant subissant alors un décalage d’un rang). L’élément \p el est
+ *  copié dans une nouvelle cellule de la liste, et l’élément \p el passé en
+ *  paramètre et l’élément dans la liste à la fin de l’exécution de cette
+ *  fonction ne sont donc pas le même objet.
+ *
+ *  \param list Liste dans laquelle est insérée le nouvel élément
+ *  \param el Nouvel élément à insérer
+ *  \param index Index auquel insérer le nouvel élément
+ */
 void list_insert(list_t *list, void *el, size_t index) {
-	struct _list_t *current, *prev, *new_ptr;
-	size_t i;
+  struct _list_t *current, *prev, *new_ptr;
+  size_t i;
 
-	list->size = (index < list->size) ? (list->size + 1) : (index + 1);
+  list->size = (index < list->size) ? (list->size + 1) : (index + 1);
 
-	prev = NULL;
-	current = list->begin;
-	for (i = 0; i < index; i++) {
-		if (current->next == NULL) {
-			current->next = malloc(sizeof *current->next);
-			current->next->data = malloc(list->data_size);
-			current->next->next = NULL;
+  prev = NULL;
+  current = list->begin;
+  for (i = 0; i < index; i++) {
+    if (current->next == NULL) {
+      current->next = malloc(sizeof *current->next);
+      current->next->data = malloc(list->data_size);
+      current->next->next = NULL;
 
-			assert(current->next);
-			assert(current->next->data);
-		}
+      assert(current->next);
+      assert(current->next->data);
+    }
 
-		prev = current;
-		current = current->next;
-	}
+    prev = current;
+    current = current->next;
+  }
 
-	new_ptr = malloc(sizeof *new_ptr);
-	new_ptr->data = malloc(list->data_size);
+  new_ptr = malloc(sizeof *new_ptr);
+  new_ptr->data = malloc(list->data_size);
 
-	assert(new_ptr);
-	assert(new_ptr->data);
+  assert(new_ptr);
+  assert(new_ptr->data);
 
-	memcpy(new_ptr->data, el, list->data_size);
+  memcpy(new_ptr->data, el, list->data_size);
 
-	if (prev != NULL)
-		prev->next = new_ptr;
-	else
-		list->begin = new_ptr;
+  if (prev != NULL) {
+    prev->next = new_ptr;
+  } else {
+    list->begin = new_ptr;
+  }
 
-	new_ptr->next = current;
+  new_ptr->next = current;
 }
 
+/**
+ *  Supprime un élément de la liste \p list situé à l’index \p index. Chaque
+ *  élément suivant l’élément supprimé verra donc son index décrémenté
+ *  de 1.\n
+ *  Attention ! Si les éléments stockés doivent être détruits via un destructeur
+ *  spécial, ce dernier ne sera pas exécuté lors de l’appel de cette fonction !
+ *
+ *  \param list Liste dans laquelle l’élément doit être détruit
+ *  \param index Index de l’élément devant être détruit
+ */
 void list_erase(list_t *list, size_t index) {
-	struct _list_t *current, *prev;
-	size_t i;
+  struct _list_t *current, *prev;
+  size_t i;
 
-	assert(index < list->size);
+  assert(index < list->size);
 
-	list->size--;
+  list->size--;
 
-	prev = NULL;
-	current = list->begin;
-	for (i = 0; i < index; i++) {
-		prev = current;
-		current = current->next;
-	}
+  prev = NULL;
+  current = list->begin;
+  for (i = 0; i < index; i++) {
+    prev = current;
+    current = current->next;
+  }
 
-	if (prev != NULL)
-		prev->next = current->next;
-	else
-		list->begin = current->next;
+  if (prev != NULL)
+    prev->next = current->next;
+  else
+    list->begin = current->next;
 
-	free(current->data);
-	free(current);
+  free(current->data);
+  free(current);
 }
 
+/**
+ *  Cette fonction remplace la valeur de l’élément de la liste \p list situé à
+ *  l’index \p index par la valeur de l’élément \p el qui sera copié.\n
+ *  Attention ! Si l’élément précédent doit être détruit via un destructeur
+ *  spécial, ce dernier ne sera pas exécuté via cette fonction !
+ *
+ *  \param list Liste dans laquelle l’élément doit être modifié
+ *  \param el Élément dont les valeurs seront copiées
+ *  \param index Index de l’élément à modifier
+ */
 void list_set(list_t list, void *el, size_t index) {
-	struct _list_t *current;
-	size_t i;
+  struct _list_t *current;
+  size_t i;
 
-	assert(index < list.size);
+  assert(index < list.size);
 
-	current = list.begin;
-	for (i = 0; i < index; i++) {
-		current = current->next;
-	}
+  current = list.begin;
+  for (i = 0; i < index; i++) {
+    current = current->next;
+  }
 
-	memcpy(current->data, el, list.data_size);
+  memcpy(current->data, el, list.data_size);
 }
 
+/**
+ *  \brief function description
+ *
+ *  Cette fonction renvoie un pointeur vers l’élément situé à l’index \p index
+ *  dans la liste \p list.
+ *
+ *  \param list Liste contenant l’élément souhaité
+ *  \param index Index de l’élément souhaité
+ *  \return Renvoie un pointeur vers l’élément d’index \p index
+ */
 void *list_get (list_t list, size_t index) {
-	struct _list_t *current;
-	size_t i;
+  struct _list_t *current;
+  size_t i;
 
-	assert(index < list.size);
+  assert(index < list.size);
 
-	current = list.begin;
-	for (i = 0; i < index; i++) {
-		current = current->next;
-	}
+  current = list.begin;
+  for (i = 0; i < index; i++) {
+    current = current->next;
+  }
 
-	return current->data;
+  return current->data;
 }
--- lib_src/GL4D/list.h
+++ lib_src/GL4D/list.h
@@ -2,8 +2,7 @@
  * \brief Fonctions de gestion de liste.
  * \author Kevin HAVRANEK mzartek@hotmail.fr
  * \date Mars 17, 2015
- *
-*/
+ */
 
 #ifndef _LIST_H
 #define _LIST_H
@@ -16,25 +15,53 @@
 extern "C" {
 #endif
 
-     typedef struct list_t list_t;
+  /*
+   * \typedef list_t
+   * `list_t` agit comme alias pour `struct list_t`
+   */
+  typedef struct list_t list_t;
 
-     struct _list_t {
-	  void *data;
-	  struct _list_t *next;
-     };
+  /*
+   * \struct _list_t
+   * \brief Wrapper pour les éléments de \ref list_t
+   *
+   * Contient un pointeur vers l’élément stocké par le wrapper et un pointeur vers
+   * le prochain élément
+   */
+  struct _list_t {
+    void *data;           /*!< Élément stocké */
+    struct _list_t *next; /*!< Pointeur vers le prochain élément */
+  };
 
-     struct list_t {
-	  struct _list_t *begin;
-	  size_t size;
-	  size_t data_size;
-     };
+  /**
+   * \struct list_t
+   * \brief Liste chaînée dynamique
+   *
+   * Wrapper de liste chaînée dynamique
+   */
+  struct list_t {
+    struct _list_t *begin; /*!< premier élément de la liste */
+    size_t size;           /*!< Nombre d’éléments dans la chaîne */
+    size_t data_size;      /*!< Taille d’un élément */
+  };
 
-     GL4DAPI list_t   GL4DAPIENTRY list_new (size_t data_size);
-     GL4DAPI void     GL4DAPIENTRY list_delete (list_t list);
-	 GL4DAPI void     GL4DAPIENTRY list_insert(list_t *list, void *el, size_t index);
-     GL4DAPI void     GL4DAPIENTRY list_erase (list_t *list, size_t index);
-     GL4DAPI void     GL4DAPIENTRY list_set (list_t list, void *el, size_t index);
-     GL4DAPI void*    GL4DAPIENTRY list_get (list_t list, size_t index);
+  /// \brief Créé une nouvelle liste
+  GL4DAPI list_t GL4DAPIENTRY list_new(size_t data_size);
+
+  /// \brief Détruit une liste
+  GL4DAPI void GL4DAPIENTRY list_delete(list_t list);
+
+  /// \brief Insère un élément dans une liste
+  GL4DAPI void GL4DAPIENTRY list_insert(list_t *list, void *el, size_t index);
+
+  /// \brief Supprime un élément d’une liste
+  GL4DAPI void GL4DAPIENTRY list_erase(list_t *list, size_t index);
+
+  /// \brief Modifie un élément dans une liste
+  GL4DAPI void GL4DAPIENTRY list_set(list_t list, void *el, size_t index);
+
+  /// \brief Retourne un pointeur vers un élément d’une liste
+  GL4DAPI void *GL4DAPIENTRY list_get(list_t list, size_t index);
 
 #ifdef __cplusplus
 }
