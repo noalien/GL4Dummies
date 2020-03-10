ed3235650085defae6843b43c8e588be797c5b61
Farès Belhadj <amsi@MacBook-Pro-de-Fares.local>
2020-03-10 11:37:21 +0100

    re-simplification

--- samples/sample2d_00-1.0/window.c
+++ samples/sample2d_00-1.0/window.c
@@ -2,8 +2,7 @@
  * \brief GL4Dummies, exemple simple 2D avec GL4Dummies
  * \author Farès BELHADJ, amsi@ai.univ-paris8.fr
  * \date February 04 2018, modified on March 06, 2020 by 
- * adding Visual Studio configuration files, a display function
- * named "draw" and a clear screen with a blue color.
+ * adding Visual Studio configuration files.
  */
 
 /* inclusion des entêtes de fonctions de gestion de primitives simples
@@ -15,12 +14,6 @@
  * fenêtres système ouvrant un contexte favorable à GL4dummies. Cette
  * partie est dépendante de la bibliothèque SDL2 */
 #include <GL4D/gl4duw_SDL2.h>
-
-/*!\brief appelée à chaque display (affichage), elle se contente de
- * raffraîchir l'ensemble de la fenêtre.*/
-static void draw(void) {
-    gl4dpUpdateScreen(NULL);
-}
 
 /*!\brief créé la fenêtre, un screen 2D effacé en noir et lance une
  *  boucle infinie.*/
@@ -35,16 +28,21 @@ int main(int argc, char ** argv) {
      * contexte OpenGL (au moins 3.2) */
     return 1;
   }
+
   /* création d'un screen GL4Dummies (texture dans laquelle nous
    * pouvons dessiner) aux dimensions de la fenêtre */
   gl4dpInitScreen();
+
   /* effacement du screen en cours en utilisant la couleur par défaut,
    * le noir */
   gl4dpClearScreen();
+
   /* décommentez la ligne suivante pour effacer l'écran en bleu foncé. */
   //gl4dpClearScreenWith(RGB(0, 0, 155));
-  /* mise en place de la fonction à appeler à chaque display */
-  gl4duwDisplayFunc(draw);
+
+  /* fonction permettant de raffraîchir l'ensemble de la fenêtre*/
+  gl4dpUpdateScreen(NULL);
+
   /* boucle infinie pour éviter que le programme ne s'arrête et ferme
    * la fenêtre immédiatement */
   gl4duwMainLoop();
