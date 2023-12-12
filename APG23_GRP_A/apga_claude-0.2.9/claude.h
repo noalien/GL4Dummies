2b4c5f726f603d17de736a15748724fb091473b4
noalien <amsi@up8.edu>
2023-12-12 11:42:37 +0100

    enable/disable + bface culling

---
+++ APG23_GRP_A/apga_claude-0.2.9/claude.h
@@ -0,0 +1,108 @@
+/*!\file claude.h
+ * \brief bibliothèque de rastérisation développée par le groupe A du
+ * cours de L2 "Algo pour la programmation graphique" (licence
+ * informatique, Univ. Paris 8) en collaboration avec l'enseignant
+ * (Farès Belhadj).
+ * \date d'octobre à fin novembre 2023
+ */
+
+#ifndef CLAUDE_H
+#define CLAUDE_H
+
+#include "primitives.h"
+
+#if SDL_BYTEORDER == SDL_BIGENDIAN 
+/*!\brief décalage pour la composante rouge */
+#  define R_SHIFT 24
+/*!\brief décalage pour la composante verte */
+#  define G_SHIFT 16
+/*!\brief décalage pour la composante bleue */
+#  define B_SHIFT  8
+/*!\brief décalage pour la composante alpha */
+#  define A_SHIFT  0
+/*!\brief masque pour la composante rouge */
+#  define R_MASK 0xff000000
+/*!\brief masque pour la composante verte */
+#  define G_MASK 0x00ff0000
+/*!\brief masque pour la composante bleue */
+#  define B_MASK 0x0000ff00
+/*!\brief masque pour la composante alpha */
+#  define A_MASK 0x000000ff
+#else
+/*!\brief décalage pour la composante rouge */
+#  define R_SHIFT  0
+/*!\brief décalage pour la composante verte */
+#  define G_SHIFT  8
+/*!\brief décalage pour la composante bleue */
+#  define B_SHIFT 16
+/*!\brief décalage pour la composante alpha */
+#  define A_SHIFT 24
+/*!\brief masque pour la composante rouge */
+#  define R_MASK 0x000000ff
+/*!\brief masque pour la composante verte */
+#  define G_MASK 0x0000ff00
+/*!\brief masque pour la composante bleue */
+#  define B_MASK 0x00ff0000
+/*!\brief masque pour la composante alpha */
+#  define A_MASK 0xff000000
+#endif
+
+#define CL_EPSILON 0.00001f
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  enum {
+    CL_OPT_BACK_FACE_CULLING = 1,
+    CL_OPT_ALPHA_BLENDING = 2,
+    CL_OPT_SHADING = 4,
+    CL_OPT_TEXTURING = 8,
+    CL_OPT_COLOR_MATERIAL = 16,
+  };
+
+  static inline uint32_t rgba(uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
+    return (((uint32_t)r) << R_SHIFT | ((uint32_t)g) << G_SHIFT |  ((uint32_t)b) << B_SHIFT | ((uint32_t)a) << A_SHIFT );
+  }
+  
+  static inline uint32_t rgb(uint8_t r, uint8_t g, uint8_t b) {
+    return rgba(r, g, b,255);
+  }
+  
+  static inline uint8_t extract_comp(uint32_t coul, int shift) {
+    return (uint8_t)((((uint32_t)(coul)) >> (shift)) & 0xFF);
+  }
+
+  static inline uint8_t red(uint32_t coul) {
+    return extract_comp(coul, R_SHIFT);
+  }
+  
+  static inline uint8_t green(uint32_t coul) {
+    return extract_comp(coul, G_SHIFT);
+  }
+  
+  static inline uint8_t blue(uint32_t coul) {
+    return extract_comp(coul, B_SHIFT);
+  }
+  
+  /* dans claude.c */
+  extern int        claude_init(int argc, char ** argv, const char * title, int ww, int wh, int width, int height);
+  extern void       claude_clear(void);
+  extern uint32_t   get_width(void);
+  extern uint32_t   get_height(void);
+  extern uint32_t * get_pixels(void);
+  extern float *    get_depths(void);
+  extern void       update_screen(void);
+  extern void claude_enable(uint32_t opt);
+  extern void claude_disable(uint32_t opt);
+  extern int  claude_is_enabled(uint32_t opt);
+  /* dans transformations.c */
+  extern void claude_apply_transforms(const mat4 M, const mat4 V, const mat4 P, const surface_t * s, surface_t * sp);
+  extern void claude_draw(surface_t * sp, const int * viewport);
+
+
+#ifdef __cplusplus
+}
+#endif
+  
+#endif /* du #ifndef CLAUDE_H */
---
+++ APG23_GRP_A/apga_claude-0.2.9/window.c
@@ -0,0 +1,144 @@
+/*!\file window.c
+ * \brief test de la lib claude
+ * \author Farès Belhadj et le groupe A de L2 (23/24)
+ * \date octobre-décembre 2023
+ */
+
+#include "claude.h"
+
+#define WW 640
+#define WH 480
+
+static void _quit(void);
+
+static surface_t * _sphere_11_11 = NULL, * _cube = NULL, * _plan = NULL;
+static texture_t * _leo = NULL, * _craq = NULL, * _rock = NULL;
+
+static void dis(void) {
+  static const int viewport[] = { 0, 0, WW, WH};
+  surface_t * s = _sphere_11_11, * sp = duplicate_surface(s);
+  surface_t * c = _cube, * cp = duplicate_surface(c);
+  surface_t * p = _plan, * pp = duplicate_surface(p);
+  vec4 rouge = { 1.0f, 0.5f, 0.5f, 0.3f };
+  vec4 vert  = { 0.5f, 1.0f, 0.5f, 1.0f };
+  vec4 bleu  = { 0.5f, 0.5f, 1.0f, 1.0f };
+  claude_clear();
+
+  mat4 projection, view, model;
+  frustum(projection, -1.0f, 1.0f, -1.0f * 3.0f / 4.0f, 1.0f * 3.0f / 4.0f, 2.0f, 150.0f);
+  mat4identite(view);
+
+  static float a0 = 0.0f, a1 = 0.0f, a2 = 0.0f, a3 = 0.0f;
+  /* même vue pour tous */
+  translate(view, 0.0f, 0.0f, -4.0f);
+  rotate(view, 20, 1.0f, 0.0f, 0.0f);
+
+  /* le cube est "petit" et devant à gauche et tourne sur xyz */
+  mat4identite(model);
+  translate(model, -0.6f, 1.0f, 1.5f * cos(a0 * 0.05f) - 2.0f);
+  scale(model, 0.2f, 0.2f, 0.2f);
+  rotate(model, a1, 0.0f, 0.0f, 1.0f);
+  rotate(model, a2, 0.0f, 1.0f, 0.0f);
+  rotate(model, a3, 1.0f, 0.0f, 0.0f);
+  claude_apply_transforms(model, view, projection, c, cp);
+  colorize_surface(cp, vert);
+  apply_texture(_craq);
+  claude_draw(cp, viewport);
+  
+  /* le plan est derrière à droite et tourne sur xyz */
+  mat4identite(model);
+  translate(model, 3.0f, 0.0f, -6.0f);
+  rotate(model, a1 += 0.21f, 0.0f, 0.0f, 1.0f);
+  rotate(model, a2 += 0.5f, 0.0f, 1.0f, 0.0f);
+  rotate(model, a3 += 0.3f, 1.0f, 0.0f, 0.0f);
+  claude_apply_transforms(model, view, projection, p, pp);
+  colorize_surface(pp, bleu);
+  apply_texture(_rock);
+  claude_disable(CL_OPT_BACK_FACE_CULLING);
+  claude_draw(pp, viewport);
+  claude_enable(CL_OPT_BACK_FACE_CULLING);
+  
+
+  /* Un exemple de plein de sphères */
+  for(int i = 0; i < 10; ++i) {
+    for(int j = 0; j < 15; ++j) {
+      mat4identite(model);
+      translate(model, (j - 7) * 4.0f, (i - 6) * 4.0f, -70.0f);
+      rotate(model, a0, 0.0f, 1.0f, 0.0f);  
+      rotate(model, a1, 1.0f, 0.0f, 0.0f);  
+      claude_apply_transforms(model, view, projection, s, sp);
+      colorize_surface(sp, bleu);
+      apply_texture(_leo);
+      claude_draw(sp, viewport);      
+    }
+  }
+
+
+  /* la sphère reste à sa place et tourne axe-y */
+  mat4identite(model);
+  rotate(model, a0 += 1.0f, 0.0f, 1.0f, 0.0f);  
+  claude_apply_transforms(model, view, projection, s, sp);
+  colorize_surface(sp, rouge);
+  apply_texture(_leo);
+  claude_draw(sp, viewport);
+  
+
+  update_screen();
+
+  free_surface(sp);
+  free_surface(cp);
+  free_surface(pp);
+}
+
+/*!\brief créé la fenêtre, un screen 2D effacé en noir et lance une
+ *  boucle infinie.*/
+int main(int argc, char ** argv) {
+  /* tentative de création d'une fenêtre pour GL4Dummies */
+  if(!claude_init(argc, argv, /* args du programme */
+		  "Claude' Hello World", /* titre */
+		  1100, 825, WW, WH) /* largeur_f, hauteur_f, largeur_e, hauteur_e */) {
+    /* ici si échec de la création souvent lié à un problème d'absence
+     * de contexte graphique ou d'impossibilité d'ouverture d'un
+     * contexte OpenGL (au moins 3.2) */
+    return 1;
+  }
+  gl4duwDisplayFunc(dis);
+  _leo = gen_texture("images/leo.bmp");
+  _craq = gen_texture("images/craq.bmp");
+  _rock = gen_texture("images/rock.bmp");
+  _sphere_11_11 = gen_sphere(11, 11);
+  _cube = gen_cube();
+  _plan = gen_quad();
+  atexit(_quit);
+  /* boucle infinie pour éviter que le programme ne s'arrête et ferme
+   * la fenêtre immédiatement */
+  gl4duwMainLoop();
+  return 0;
+}
+
+static void _quit(void) {
+  if(_sphere_11_11) {
+    free_surface(_sphere_11_11);
+    _sphere_11_11 = NULL;
+  }
+  if(_cube) {
+    free_surface(_cube);
+    _cube = NULL;
+  }
+  if(_plan) {
+    free_surface(_plan);
+    _plan = NULL;
+  }
+  if(_leo) {
+    free_texture(_leo);
+    _leo = NULL;
+  }
+  if(_craq) {
+    free_texture(_craq);
+    _craq = NULL;
+  }
+  if(_rock) {
+    free_texture(_rock);
+    _rock = NULL;
+  }
+}
